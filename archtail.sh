33e75338dcd951e5fc8cfe36e7bf4227285d7f66
David S. Jackson <deepbsd@yahoo.com>
2021-04-16 10:56:35 -0400

    updated archtail

--- archtail.sh
+++ archtail.sh
@@ -36,6 +36,55 @@ HOME_SIZE=12G   # This is set automatically if using LVM
 TIMEZONE=$(wget -O - -q http://geoip.ubuntu.com/lookup | sed -n -e 's/.*<TimeZone>\(.*\)<\/TimeZone>.*/\1/p')
 LOCALE="en_US.UTF-8"
 
+###########  SOFTWARE SETS ###################
+
+# replace with linux-lts or -zen if preferrable
+base_system=( base base-devel linux linux-headers dkms linux-firmware vim sudo bash-completion )
+
+base_essentials=(git mlocate pacman-contrib man-db man-pages)
+
+network_essentials=( iwd dhcpcd openssh networkmanager )
+
+my_services=( dhcpcd sshd NetworkManager systemd-homed )
+
+basic_x=( xorg-server xorg-xinit mesa xorg-twm xterm gnome-terminal xorg-xclock xfce4-terminal firefox neofetch screenfetch lightdm-gtk-greeter )
+
+extra_x1=( gkrellm powerline powerline-fonts powerline-vim adobe-source-code-pro-fonts cantarell-fonts gnu-free-fonts ) 
+
+extra_x2=( noto-fonts breeze-gtk breeze-icons gtk-engine-murrine oxygen-icons xcursor-themes adapta-gtk-theme )
+
+extra_x3=( arc-gtk-theme elementary-icon-theme faenza-icon-theme gnome-icon-theme-extras arc-icon-theme lightdm-webkit-theme-litarvan mate-icon-theme ) 
+
+extra_x4=( materia-gtk-theme papirus-icon-theme xcursor-bluecurve xcursor-premium archlinux-wallpaper deepin-community-wallpapers deepin-wallpapers elementary-wallpapers )
+
+cinnamon_desktop=( cinnamon nemo-fileroller )
+
+#####  Include in 'all_extras' array if desired
+xfce_desktop=( xfce4 xfce4-goodies )
+
+mate_desktop=( mate mate-extra )
+
+i3gaps_desktop=( i3-gaps dmenu feh rofi i3status i3blocks nitrogen i3status ttf-font-awesome ttf-ionicons )
+
+qtile_desktop=( qtile )
+
+kde_desktop=( lightdm-kde-greeter plasma plasma-wayland-session kde-applications )
+
+## Python3 should be installed by default
+devel_stuff=( git nodejs npm npm-check-updates ruby )
+
+printing_stuff=( system-config-printer foomatic-db foomatic-db-engine gutenprint cups cups-pdf cups-filters cups-pk-helper ghostscript gsfonts )
+
+multimedia_stuff=( brasero sox cheese eog shotwell imagemagick sox cmus mpg123 alsa-utils cheese )
+
+all_extras=( "${xfce_desktop[@]}" "${i3gaps_desktop[@]}" "${mate_desktop[@]}" "${devel_stuff[@]}" "${printing_stuff[@]}" "${multimedia_stuff[@]}" )
+
+##  fonts_themes=()    #  in case I want to break these out from extra_x
+
+# This will exclude services because they are often named differently and are duplicates
+all_pkgs=( base_system base_essentials network_essentials basic_x extra_x1 extra_x2 extra_x3 extra_x4 cinnamon_desktop xfce_desktop mate_desktop i3gaps_desktop devel_stuff printing_stuff multimedia_stuff qtile_desktop kde_desktop )
+
+completed_tasks=()
 
 
 ##################################
