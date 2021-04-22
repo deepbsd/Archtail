c684845e3ca450720d732f534a16aef3b38754f4
David S. Jackson <deepbsd@yahoo.com>
2021-04-22 11:54:50 -0400

    updated --inputbox messages in part_disk

--- archtail.sh
+++ archtail.sh
@@ -366,23 +366,21 @@ EOF
 
 
     # EFI_DEVICE
-    efi_dev_message=$(echo "EFI device name (leave empty if not EFI/GPT) (sda1, nvmen1p1, etc)?" && lsblk -f "$IN_DEVICE")
+    efi_dev_message=$(echo "EFI device name (leave empty if not EFI/GPT) (sda1, nvmen1p1, etc)?")
     efi_device=$(whiptail --title "Get EFI Device Name" --inputbox "$efi_dev_message" 30 75 3>&1 1>&2 2>&3)
     EFI_SLICE="/dev/$efi_device"
     [[ -n "$efi_device" ]] && format_disk "$EFI_SLICE" efi
 
     # SWAP_DEVICE
-    swap_dev_message=$(lsblk -f "$IN_DEVICE" && echo "Swap device name? (sda3, nvmen1p3, etc) (leave empty if no swap device)")
+    swap_dev_message=$(echo "Swap device name? (sda3, nvmen1p3, etc) (leave empty if no swap device)")
     swap_device=$(whiptail --title "Get Swap Device" --inputbox "$swap_dev_message" 30 75 3>&1 1>&2 2>&3)
     SWAP_SLICE="/dev/$swap_device"
-    #echo "Formatting $SWAP_SLICE" && sleep 2
     [[ -n "$swap_device" ]] && format_disk "$SWAP_SLICE" swap
 
     # HOME_DEVICE
-    home_dev_message=$(echo "Home device name? (sda4, nvmen1p4, etc) (leave empty if no home device)" && lsblk -f "$IN_DEVICE")
+    home_dev_message=$(echo "Home device name? (sda4, nvmen1p4, etc) (leave empty if no home device)")
     home_device=$(whiptail --title "Get Home Device" --inputbox "$home_dev_message" 30 75 3>&1 1>&2 2>&3)
     HOME_SLICE="/dev/$home_device"
-    #echo "Formatting $HOME_SLICE" && sleep 2
     [[ -n "$home_device" ]] && format_disk "$HOME_SLICE" home
 
     # CHECK IF IT HAPPENED CORRECTLY
