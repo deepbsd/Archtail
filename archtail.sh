b75f92a2639df0311a0d3625c9200c3f252ab3f8
David S. Jackson <deepbsd@yahoo.com>
2021-05-01 19:53:35 -0400

    updating program

--- archtail.sh
+++ archtail.sh
@@ -674,7 +674,7 @@ startmenu(){
             "H")  set_hostname; check_tasks 5 ;;
             "R")  password=$(whiptail --passwordbox "Please set your new root password..." --backtitle "SETTING ROOT PASSWORD" --title "Set new root password"   8 48 3>&1 1>&2 2>&3);
                   echo -e "$password\n$password" | arch-chroot /mnt passwd ;; 
-            "M")  specialprogressgauge install_essential "Installing dhcpcd, sshd, ssh, networkmanager" ; 
+            "M")  specialprogressgauge install_essential "Installing dhcpcd, sshd, ssh, networkmanager, etc..." ; 
                   whiptail --title "Network Essentials Installed" --msgbox "Network Essentials Installed.  OK to continue." 8 78;
                   whiptail --title "Current Install Progress" --textbox /tmp/install.log --scrolltext 25 80;
                   check_tasks 7 ;;
