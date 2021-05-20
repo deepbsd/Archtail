7b8e8c11d79384da86c90d3f01fd150d01b38e50
David S. Jackson <deepbsd@yahoo.com>
2021-05-20 14:56:51 -0400

    changed note on diskmenu

--- archtail.sh
+++ archtail.sh
@@ -642,7 +642,7 @@ diskmenu(){
         "N"   "Prepare Installation Disk with Normal Partitions" \
         "L"   "Prepare Installation Disk with LVM"   \
         "E"   "Prepare Installation Disk Encryption and LVM"   \
-        "M"   "Manually Partition Disk with cfdisk (experimental)"   \
+        "M"   "Manually Partition Disk with cfdisk (not yet functioning)"   \
         "R"   "Return to previous menu"   3>&1 1>&2 2>&3 ) 
 
         case $diskmenupick in
