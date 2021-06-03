d41af6b75e33cac4298560c2f0c5fab9b317730a
David S. Jackson <deepbsd@yahoo.com>
2021-06-02 21:21:46 -0400

    set default for KEYBOARD even if user chooses to select another keymap file

--- archtail.sh
+++ archtail.sh
@@ -186,6 +186,7 @@ change_kb(){
         # if not, there are about 260 other options...
         KEYBOARD=$(eval resize; whiptail --backtitle "CHOOSE KEYBOARD" --title "Choose Your Keyboard" \
             --menu "Default keymap is US"  $LINES $COLUMNS $(( $LINES - 8 )) "${options[@]}" 3>&1 1>&2 2>&3 )
+        KEYBOARD=${KEYBOARD:='us'}
     fi
 
     echo "==> Using $KEYBOARD keyboard keymap..." &>>$LOGFILE
