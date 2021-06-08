4b6018d0d1b997cad1d8800943020df5927672c6
David S. Jackson <deepbsd@yahoo.com>
2021-06-07 23:47:55 -0400

    working on change_locale

--- archtail.sh
+++ archtail.sh
@@ -936,10 +936,11 @@ set_locale(){
     else
         # Here's the array of available locales:
         for locale in $(egrep '^#?[a-z]{2}_*' /etc/locale.gen | sed 's/^#//g'); do
-            LOCALES+=($locale)
+            LOCALES+=( $(printf "%s\t\t%s\n" $locale "=======")  )
         done
 
         # Come up with a whiptail selection menu of all available locales on system
+        echo "${LOCALES[@]}"
 
     fi
 
